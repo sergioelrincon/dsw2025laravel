9b369fdb201e9418dbf9fb59956109825062f940
Sergio Ramos <sergioramos@ieselrincon.es>
2026-03-03 19:17:24 +0000

    Implementamos una API de demostración para listar los mensajes y añadir uno nuevo.

---
+++ app/Http/Controllers/Api/V1/MensajeController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Message;
+
+class MensajeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // Obtenemos todos los mensajes de la base de datos
+        $mensajes = Message::all();
+
+        // Retornamos los mensajes en formato JSON. 
+        return response()->json([
+            'status' => 'success',
+            'data' => $mensajes
+        ], 200);
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+
+        // Validamos los datos recibidos
+        $datos = $request->validate([
+            'nombre' => 'required|string|max:50',
+            'email' => 'required|email',
+            'mensaje' => 'required|string|max:500',
+        ]);
+
+        // Insertamos un nuevo mensaje en la base de datos.
+        $mensaje = new Message();
+        $mensaje->name = $datos['nombre'];
+        $mensaje->email = $datos['email'];
+        $mensaje->message = $datos['mensaje'];
+        $mensaje->save();
+
+        // Retornamos el mensaje creado en formato JSON con un código de estado 201 (creado).
+        return response()->json([
+            'status' => 'success',
+            'data' => $mensaje
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// Rutas necesarias para implementar una API que permita consultar los mensajes y añadir nuevos mensajes. Estas rutas se corresponden con los métodos del controlador MensajeController:
+Route::prefix('v1')->group(function () {
+    Route::get('/mensajes', [App\Http\Controllers\Api\V1\MensajeController::class, 'index'])->name('mensaje.index');
+    Route::post('/mensajes', [App\Http\Controllers\Api\V1\MensajeController::class, 'store'])->name('mensaje.store');
+});
